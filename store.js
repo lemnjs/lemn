68e89add8f9e69b584294de9fad90a637356d8a4
Michael "Z" Goddard <mzgoddard@gmail.com>
2019-01-03 18:04:44 -0500

    move index.js to render.js; add store.js

---
+++ store.js
@@ -0,0 +1,121 @@
+import {rerender} from './render';
+
+export class Store {
+    constructor (data = {}) {
+        this.data = {};
+        for (const key in data) {
+            this.data[key] = new Model(data[key]);
+        }
+    }
+
+    get (id) {
+        return id in this.data ? this.data[id].data : {};
+    }
+
+    set (id, data) {
+        const model = this.data[id] = this.data[id] || new Model();
+        model.set(data);
+    }
+
+    connect (id, fn) {
+        const model = this.data[id] = this.data[id] || new Model();
+        return new Connect(model, fn);
+    }
+
+    toJSON () {
+        return this.data;
+    }
+}
+
+class Model {
+    constructor (data = {}) {
+        this.activeChildren = [];
+        this.data = data;
+    }
+
+    set (data) {
+        this.data = data;
+        this.activeChildren.forEach(child => child.update());
+    }
+
+    unbind (child) {
+        this.activeChildren = this.activeChildren.filter(c => c !== child);
+    }
+
+    bind (child) {
+        this.activeChildren = [...this.activeChildren, child];
+    }
+
+    maybeInit () {}
+
+    toJSON () {
+        return this.data;
+    }
+}
+
+class Connect {
+    constructor (parent, fn) {
+        this.parent = parent;
+        this.fn = fn;
+        this.activeChildren = null;
+        this.oldParentData = null;
+        this.data = null;
+        this.maybeInit();
+    }
+
+    willDetach () {
+        if (this.attached) {
+            this.parent.unbind(this);
+            this.attached = false;
+            this.oldParentData = null;
+            this.data = null;
+        }
+    }
+
+    didAttach () {
+        if (!this.attached) {
+            this.attached = true;
+            this.parent.bind(this);
+        }
+    }
+
+    connect (fn) {
+        return new Connect(this, fn);
+    }
+
+    unbind (child) {
+        this.activeChildren = this.activeChildren.filter(c => c !== child);
+        this.parent.unbind(this);
+    }
+
+    bind (child) {
+        this.parent.bind(this);
+        this.activeChildren = [...(this.activeChildren || []), child];
+    }
+
+    maybeInit () {
+        this.parent.maybeInit();
+        if (this.oldParentData !== this.parent.data) {
+            this.oldParentData = this.parent.data;
+            this.data = this.fn(this.parent.data, this.data) || this.data;
+        }
+    }
+
+    update () {
+        if (this.oldParentData !== this.parent.data) {
+            this.oldParentData = this.parent.data;
+            const out = this.fn(this.parent.data, this.data);
+            if (out !== null) {
+                this.data = out;
+                (this.activeChildren || []).forEach(child => child.update());
+                if (this.attached) {
+                    rerender(this);
+                }
+            }
+        }
+    }
+
+    render () {
+        return this.data;
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,18 @@ module.exports = {
     devtool: '',
     target: 'web',
     stats: process.env.WEBPACK_STDOUT ? 'none' : 'normal',
-    entry: 'shebang-loader!./index.js',
+    entry: './index.js',
     output: {
         path: `${__dirname}/tmp/test`,
         filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: `shebang-loader`,
+            },
+        ],
     },
     plugins: [
         // new (require('webpack-visualizer-plugin'))(),
@@ -24,17 +32,77 @@ module.exports = {
         }),
         process.env.WEBPACK_STDOUT ? {
             apply (compiler) {
+                compiler.hooks.compilation.tap('name', compilation => {
+                    compilation.mainTemplate.hooks.render.tap('name', (lastSource) => {
+                        // return lastSource;
+                        // console.log('a');
+                        try {
+                        const ConcatSource = require('webpack-sources/lib/ConcatSource');
+                        const OriginalSource = require('webpack-sources/lib/OriginalSource');
+                        const source = new ConcatSource();
+                        source.add('(function ([factory]) {');
+                        source.add(new OriginalSource([
+                            'const __webpack_require__ = {r () {}, d (exports, name, getter) {Object.defineProperty(exports, {enumerable: false, get: getter});}};',
+                            // 'const __webpack_require__ = function () {};',
+                            // '__webpack_require__.r = __webpack_require__;',
+                            // '__webpack_require__.d = function (exports, name, getter) {Object.defineProperty(exports, {enumerable: false, get: getter});};',
+                            'const exports = {};',
+                            'factory.call(exports, null, exports, __webpack_require__);',
+                            'return exports;'
+                        ].join('\n'), 'cheap bootstrap'));
+                        source.add('})');
+                        source.add('(');
+                        // console.log();
+                        // const modules = new ConcatSource();
+                        lastSource.children.slice(5).forEach(s => source.add(s));
+                        // source.add(modules);
+                        // source.add(lastSource.children[5]);
+                        // source.add(')');
+                        // console.log('b');
+                        // console.log(source.source());
+                        return source;
+                        } catch (e) {
+                            console.error(e);
+                        }
+                    });
+                    // compilation.mainTemplate.hooks.requireExtensions.tap('name', () => {
+ //                        return [
+ //                        '// define getter function for harmony exports',
+ //                        '__webpack_require__.d = function(exports, name, getter) {',
+ //                        '    if(!__webpack_require__.o(exports, name)) {',
+ //                        '        Object.defineProperty(exports, name, { enumerable: true, get: getter });',
+ //                        '    }',
+ //                        '};',
+ //                        '',
+ //                        '// define __esModule on exports',
+ //                        '__webpack_require__.r = function(exports) {',
+ //                        '    if(typeof Symbol !== \'undefined\' && Symbol.toStringTag) {',
+ //                        '        Object.defineProperty(exports, Symbol.toStringTag, { value: \'Module\' });',
+ //                        '    }',
+ //                        '    Object.defineProperty(exports, \'__esModule\', { value: true });',
+ //                        '};',
+ //                        '',
+ //                        '// Object.prototype.hasOwnProperty.call',
+ //                        '__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };',
+ //                        ].join('\n');
+ //                    });
+                });
                 compiler.hooks.afterCompile.tap('name', compilation => {
+                    if (Object.keys(compilation.assets).length === 0) {
+                        console.error('no assets');
+                        return;
+                    }
                     // development module
                     if (process.env.NODE_ENV === 'development') {
-                        console.log(compilation.assets['main.js']._source.children[12].source());
+                       // console.log((compilation.assets['main.js']._source.children._source.children[12]);
+                       console.log(compilation.assets['main.js']._source.children[12].source());
                     }
 
                     // production module
                     // console.log(compilation.modules[0]._cachedSources.get('javascript').source._cachedSource);
 
                     // full source, used in testing
-                    if (process.env.NODE_ENV === 'test') {
+                    else {
                         console.log(compilation.assets['main.js'].source());
                     }
                     Object.keys(compilation.assets).forEach(key => {
